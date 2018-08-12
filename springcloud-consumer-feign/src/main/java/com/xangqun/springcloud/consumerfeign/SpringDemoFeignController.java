513824698ead71cd6ae453e29fbf818ba7d38763
xangqun <294595601@qq.com>
2018-08-12 16:02:48 +0800

    chome:1.添加异步实现

--- springcloud-consumer-feign/src/main/java/com/xangqun/springcloud/consumerfeign/SpringDemoFeignController.java
+++ springcloud-consumer-feign/src/main/java/com/xangqun/springcloud/consumerfeign/SpringDemoFeignController.java
@@ -5,14 +5,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class SpringDemoFeignController {
 
     @Autowired
     SpringDemoFeignService springDemoFeignService;
 
+    @Autowired
+    private Task task;
+
     @RequestMapping(value = "hello", method = RequestMethod.GET)
-    public String port() {
+    public String port() throws Exception{
+        Future<String> futureResult = task.run();
+        String result = futureResult.get(5, TimeUnit.SECONDS);
         return springDemoFeignService.hello();
     }
+
+
 }
