bb42a0cd68b7357aa9b7a299894ff9c256b48eee
laixiangqun <laixiangqun@hdsc.com>
2018-08-14 09:13:58 +0800

    chome:文件上传实现

--- springcloud-consumer-feign/src/main/java/com/xangqun/springcloud/consumerfeign/kaptcha/KaptchaController.java
+++ springcloud-consumer-feign/src/main/java/com/xangqun/springcloud/consumerfeign/kaptcha/KaptchaController.java
@@ -11,12 +11,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * @author laixiangqun
@@ -54,4 +62,42 @@ public class KaptchaController {
         ImageIO.write(image, "JPEG", out);
         IOUtils.closeQuietly(out);
     }
+
+    /**
+     * 文件上传实现
+     * @param session
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping("/doUpload")
+    public String doUpload(HttpSession session, HttpServletRequest request) throws IOException {
+        long startTime = System.currentTimeMillis();
+        //获取存储app文件夹的路径
+        String appPath = session.getServletContext().getRealPath("/app");
+        File appRootDir = new File(appPath);
+        if (!appRootDir.exists()) {
+            System.out.println("存储app的文件夹不存在 appPath= " + appPath);
+            appRootDir.mkdirs();
+        } else {
+            System.out.println("存储app的文件夹存在 appPath= " + appPath);
+        }
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        if (multipartResolver.isMultipart(request)) {
+            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+            Iterator<String> names = multiRequest.getFileNames();
+            while (names.hasNext()) {
+                MultipartFile file = multiRequest.getFile(names.next());
+                if (file != null) {
+                    File appFile = new File(appRootDir, file.getOriginalFilename());
+                    file.transferTo(appFile);
+                }
+            }
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("上传时间：" + String.valueOf(endTime - startTime) + "ms");
+        return "home";
+    }
+
 }
